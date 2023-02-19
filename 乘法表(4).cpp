22abf25e796eb2092bf8026964f30e9f7c40690b
Jack666Yue <jack666noip@gitcode.net>
2023-02-19 11:15:18 +0800

    11

--- 乘法表(4).cpp
+++ 乘法表(4).cpp
@@ -6,7 +6,7 @@ int main()
   for(int i=1; i<=9; i++)
   {
     for(int j=1; j<=i; j++)
-      cout<<i<<" ¡Á "<<j<<" = "<<i*j<<" ";
+      cout<<i<<" * "<<j<<" = "<<i*j<<" ";
     cout<<endl;
   }
   return 0;
