6d787fdbdbf7156ad0684acf050109862971e32f
Jack666Yue <jack666noip@gitcode.net>
2023-02-26 09:31:51 +0800

    Feb.26th.2023

--- n-皇后问题.cpp
+++ n-皇后问题.cpp
@@ -1,36 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int N = 20;
-int n;
-char g[N][N];
-bool col[N], dg[N], udg[N];
+#include <iostream>
 
-void DFS(int u) {
-    if (u == n) {
-        for (int i = 0; i < n; i++) puts(g[i]);
-        puts("");
-        return ;
-    }
-    for (int i = 0; i < n; i++) {
-        if (!col[i] && !dg[u + i] && !udg[n - u + i]) {
-            g[u][i] = 'Q';
-            col[i] = dg[u + i] = udg[n - u + i] = true;
-            DFS(u + 1);
-            col[i] = dg[u + i] = udg[n - u + i] = false;
-			g[u][i] = '.';
+using namespace std;
+
+const int N = 10;
+
+int n;
+bool row[N], col[N], dg[N * 2], udg[N * 2];
+char g[N][N];
+
+void dfs(int x, int y, int s)
+{
+    if (s > n) return;
+    if (y == n) y = 0, x ++ ;
+
+    if (x == n)
+    {
+        if (s == n)
+        {
+            for (int i = 0; i < n; i ++ ) printf("%s\n",g[i]);
+            printf("\n");
         }
+        return;
+    }
+
+    g[x][y] = '.';
+    dfs(x, y + 1, s);
+
+    if (!row[x] && !col[y] && !dg[x + y] && !udg[x - y + n])
+    {
+        row[x] = col[y] = dg[x + y] = udg[x - y + n] = true;
+        g[x][y] = 'Q';
+        dfs(x, y + 1, s + 1);
+        g[x][y] = '.';
+        row[x] = col[y] = dg[x + y] = udg[x - y + n] = false;
     }
 }
 
-int main() {
-    //freopen(".in","r",stdin);
-    //freopen(".out","w",stdout);
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; i < n; i++) {
-            g[i][j] = '.';
-        }
-    }
-	DFS(0);
+int main()
+{
+    cin >> n;
+
+    dfs(0, 0, 0);
+
     return 0;
 }
